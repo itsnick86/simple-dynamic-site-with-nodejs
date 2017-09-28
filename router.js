360d876e15a2e8273a50d63483adc5ddaf9f5269
Nick Edwards <nledwa01@gmail.com>
2017-09-27 22:04:08 -0400

    Set up user router to pull JSON data

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 //Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   //if url == "/" && GET
@@ -17,22 +19,39 @@ function home(request, response) {
 
 //Handle HTTP route GET / :username i.e. /chalkers
 function user(request, response) {
-    //if url == "/..."
-    var username = request.url.replace("/", "");
-    if(username.length > 0) {
-      response.statusCode = 200;
-      response.setHeader('Content-Type', 'text/plain');
-      response.write("Header\n");
-      response.write(username + "\n");
+  //if url == "/..."
+  var username = request.url.replace("/", "");
+  if(username.length > 0) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.write("Header\n");
+
+    //get json from Treehouse
+    var studentProfile = new Profile(username);
+    //on "end"
+    studentProfile.on("end", function(profileJSON) {
+      //show profile
+
+      //Store the values which we need
+      var values = {
+        avatarURL: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      //Simple Response
+      response.write(values.username + " has " + values.badges + " badges\n");
       response.end("Footer\n");
+    });
 
+    //on "error"
+    studentProfile.on("error", function(error) {
+      //show error
+      response.write(error.message + "\n")
+      response.end("Footer\n");
+    });
 
-      //get json from Treehouse
-        //on "end"
-          //show profile
-        //on "error"
-          //show error
-    }
+  }
 }
 
 module.exports.home = home;
